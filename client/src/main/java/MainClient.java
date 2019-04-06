80b7da6fb8730ce7b75ae5d21b87ef430d6aeb77
Raul <raulbc099@gmail.com>
2019-04-06 20:47:02 +0300

    gata, poti sa te apuci de lucru

--- client/src/main/java/MainClient.java
+++ client/src/main/java/MainClient.java
@@ -20,7 +20,7 @@ public class MainClient {
         Client_MovieService client_movieService=context.getBean(Client_MovieService.class);
         Client_RentalService client_rentalService=context.getBean(Client_RentalService.class);
         Console console=context.getBean(Console.class);
-        console.setServices(client_movieService,client_clientService,client_rentalService);
+        console.setSerivces(client_movieService,client_clientService,client_rentalService);
         console.run();
     }
 }
--- client/src/main/java/ui/Console.java
+++ client/src/main/java/ui/Console.java
@@ -1,29 +1,27 @@
 package ui;
 
-
 import common.domain.Client;
 import common.domain.Movie;
 import common.domain.Rental;
 import common.domain.exceptions.*;
+import common.message.utils.MockPredicate;
 import common.service.ClientService;
 import common.service.MovieService;
 import common.service.RentalService;
-
-import java.util.Comparator;
-
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.*;
 import java.util.stream.Stream;
-
 import static common.domain.FilterPredicates.*;
-
+import static java.lang.Thread.sleep;
 
 public class Console {
     private MovieService movieService;
@@ -35,15 +33,17 @@ public class Console {
         this.movieService = movieService;
         this.clientService = clientService;
         this.rentalService = rentalService;
+
     }
 
     public Console() {
+
     }
 
-    public void setServices(MovieService movieService, ClientService clientService, RentalService rentalService) {
-        this.movieService = movieService;
-        this.clientService = clientService;
-        this.rentalService = rentalService;
+    public void setSerivces(MovieService movieService,ClientService clientService,RentalService rentalService){
+        this.movieService=movieService;
+        this.clientService=clientService;
+        this.rentalService=rentalService;
     }
 
     private void printCommands() {
@@ -72,6 +72,8 @@ public class Console {
         System.out.println("22. View clients paged");
         System.out.println("23. View Rentals paged");
         System.out.println("24. SetClientPageSize");
+        System.out.println("25. Flood movies");
+        System.out.println("26. Flood clients");
     }
 
     public void run() {
@@ -164,64 +166,85 @@ public class Console {
                 ex.printStackTrace();
             } catch (ClientNotFound | MovieNotFound | RentalNotAdded cnf) {
                 System.out.println(cnf.getMessage());
-            } catch (Exception ex) {
+            }catch (IOException ioe) {
+                System.out.println("IOException");
+                ioe.printStackTrace();
+            }
+            catch (Exception ex) {
                 System.out.println("An unknown exception has occured");
                 ex.printStackTrace();
             }
         }
     }
 
+
     private void setClientPageSize() {
         int pageSize=readInt("Enter the page size:");
-        clientService.setPageSize(pageSize);
+        CompletableFuture.runAsync(()-> {
+            clientService.setPageSize(pageSize);
+        });
     }
 
     private void bestMovies() {
-
-        Map<Movie, Set<Rental>> mMap=rentalService.getMovieRental();
-        mMap.entrySet().stream().sorted(Comparator.comparingInt(o -> o.getValue().size()*(-1))).limit(3)
-                .forEach((c)->System.out.println(c.getKey().getTitle()+" - "+c.getValue().size()));
+        CompletableFuture.runAsync(()-> {
+            rentalService.getMovieRental().entrySet().stream()
+                    .sorted(Comparator.comparingInt(o -> o.getValue().size() * (-1))).limit(3)
+                    .forEach((c) -> System.out.println(c.getKey().getTitle() + " - " + c.getValue().size()));
+        });
     }
 
     private void bestClients(){
 
-        Map<Client,Set<Rental>> cMap=rentalService.getClientRental();
-        cMap.entrySet().stream().sorted(Comparator.comparingInt(o -> o.getValue().size()*(-1))).limit(3)
-                .forEach((c)->System.out.println(c.getKey().getName()+" - "+c.getValue().size()));
+        CompletableFuture.runAsync(()-> {
+
+            rentalService.getClientRental().entrySet().stream().sorted(Comparator.comparingInt(o -> o.getValue().size() * (-1))).limit(3)
+                    .forEach((c) -> System.out.println(c.getKey().getName() + " - " + c.getValue().size()));
+        });
     }
 
-    private void updateMovie() {
+    private void updateMovie() throws IOException{
+
         int id=readInt("Give movie ID:");
-        Optional<Movie> iMovie=movieService.findOne(id);
-        iMovie.orElseThrow(()->new MovieNotFound("Movie does not exist"));
-        Movie mov=iMovie.get();
+        System.out.println("Enter the new title:");
+        String title=reader.readLine();
+        System.out.println("Enter the new Genre:");
+        String genre=reader.readLine();
         float newRating=readFloat("Enter the new rating:");
-        Optional<Movie> m=movieService.update(new Movie(mov.getId(),mov.getTitle(),mov.getGenre(),newRating));
-        m.ifPresent(mi->System.out.println("Movie updated"));
+        CompletableFuture.runAsync(()->{
+            Optional<Movie> mo=movieService.update(new Movie(id,title,genre,newRating));
+            mo.ifPresent(mi->System.out.println("Movie updated"));
+            mo.orElseThrow(()->new MovieNotFound("Movie not found"));
+
+        });
     }
 
-    private void updateClient(){
+    private void updateClient() throws IOException{
         long id=readInt("Give client ID:");
-        Optional<Client> iClient=clientService.findOne(id);
-        iClient.orElseThrow(()->new ClientNotFound("Client does not exist"));
-        Client client=iClient.get();
+        System.out.println("Enter the new name:");
+        String name=reader.readLine();
+        System.out.println("Enter the new email:");
+        String email=reader.readLine();
         int newAge=readInt("Enter the new age:");
-        Optional<Client> m=clientService.update(new Client(client.getId(),client.getName(),client.getEmail(),newAge));
-        m.ifPresent(mi->System.out.println("Client updated"));
-
+        CompletableFuture.runAsync(()->{
+            Optional<Client> mo=clientService.update(new Client(id,name,email,newAge));
+            mo.ifPresent(mi->System.out.println("Client updated"));
+            mo.orElseThrow(()->new ClientNotFound("Client not found"));
+        });
     }
 
     private void clientStatistics(){
-      Map<Client,Set<Rental>> clientRentals=rentalService.getClientRental();
-      System.out.println("Client name - Number of rentals");
-      clientRentals.forEach((c,s)->System.out.println(c.getName()+" - "+s.size()));
 
+      CompletableFuture.runAsync(()->{
+          System.out.println("Client name - Number of rentals");
+          rentalService.getClientRental().forEach((c,s)->System.out.println(c.getName()+" - "+s.size()));
+      });
     }
 
     private  void movieStatistics(){
-        Map<Movie,Set<Rental>> movieRentals=rentalService.getMovieRental();
-        System.out.println("Movie name - Number of rentals");
-        movieRentals.forEach((c,s)->System.out.println(c.getTitle()+" - "+s.size()));
+        CompletableFuture.runAsync(()->{
+            System.out.println("Movie name - Number of rentals");
+            rentalService.getMovieRental().forEach((c,s)->System.out.println(c.getTitle()+" - "+s.size()));
+        });
     }
 
     private Client readClient() throws IOException {
@@ -254,78 +277,108 @@ public class Console {
     private void addMovie() throws IOException{
 
         Movie m = readMovie();
-        Optional<Movie> aMovie = movieService.addMovie(m);
-        try {
-            aMovie.ifPresent(mo -> System.out.println("Movie already exists"));
-            aMovie.orElseThrow(() -> new MovieAdded("Movie added"));
-        } catch (MovieAdded ma) {
-            System.out.println(ma.getMessage());
-        }
+
+        CompletableFuture.runAsync(()->{
+            try {
+                Optional<Movie> mo = movieService.addMovie(m);
+                mo.ifPresent(mi -> System.out.println("Movie already exists"));
+                mo.orElseThrow(() -> new MovieAdded("Movie added"));
+            } catch (MovieAdded ma) {
+                System.out.println(ma.getMessage());
+            }
+        });
+
     }
 
     private void listAllMovies() {
-        movieService.getAllMovies().forEach(m -> System.out.println(m.toString()));
+        CompletableFuture.runAsync(() -> {
+            movieService.getAllMovies().forEach(m -> System.out.println(m.toString()));
+        });
     }
 
     private void listAllMoviesPaged(){
-        Stream<Movie> movies = movieService.findAllPaged();
 
-        movies.forEach(System.out::println);
+        CompletableFuture.runAsync(() -> {
+            movieService.findAllPaged().forEach(System.out::println);
+        });
 
     }
 
     private void listAllClientsPaged(){
-        Stream<Client> clients=clientService.findAllPaged();
-        clients.forEach(System.out::println);
+
+        CompletableFuture.runAsync(()->{
+            clientService.findAllPaged().forEach(System.out::println);
+        });
     }
 
     private void listAllRentalsPaged(){
-        Stream<Rental> rentals=rentalService.findAllPaged();
-        rentals.forEach(System.out::println);
+
+        CompletableFuture.runAsync(()->{
+            rentalService.findAllPaged().forEach(System.out::println);
+        });
     }
 
     private void deleteMovie() {
         int ID = readInt("Give movie ID:");
-        Optional<Movie> dMovie = movieService.deleteMovie(ID);
-        dMovie.ifPresent(m -> System.out.println("Movie deleted"));
-        dMovie.orElseThrow(() -> new MovieNotFound("Movie not found, nothing deleted"));
+
+        CompletableFuture.runAsync(()->{
+            try {
+                Optional<Movie> mo=movieService.deleteMovie(ID);
+                mo.ifPresent(m -> System.out.println("Movie deleted"));
+                mo.orElseThrow(() -> new MovieNotFound("Movie not found, nothing deleted"));
+            }catch (MovieNotFound mnf){
+                System.out.println(mnf.getMessage());
+            }
+        });
 
     }
 
     private void addClient() throws IOException{
         Client c = readClient();
-        Optional<Client> aClient = clientService.addClient(c);
-        try {
-            aClient.ifPresent(cl -> System.out.println("Client already exists"));
-            aClient.orElseThrow(() -> new ClientAdded("Client added"));
-        } catch (ClientAdded ca) {
-            System.out.println(ca.getMessage());
-        }
+
+        CompletableFuture.runAsync(()->{
+            try {
+                Optional<Client> mo=clientService.addClient(c);
+                mo.ifPresent(cl -> System.out.println("Client already exists"));
+                mo.orElseThrow(() -> new ClientAdded("Client added"));
+            } catch (ClientAdded ca){
+                System.out.println(ca.getMessage());
+            }
+        });
     }
 
     private void listAllClients() {
-        clientService.getAllClients().forEach(c -> System.out.println(c + "\n"));
+        CompletableFuture.runAsync(()-> {
+            clientService.getAllClients().forEach(c -> System.out.println(c + "\n"));
+        });
     }
 
     private void removeClient() {
         long cnp;
         boolean ok = false;
-        while (!ok) {
-            try {
-                System.out.print("Give client CNP:");
-                cnp = Long.parseLong(reader.readLine());
-                Optional<Client> dClient = clientService.deleteClient(cnp);
-                dClient.ifPresent(c -> System.out.println("Client deleted"));
-                dClient.orElseThrow(() -> new ClientNotFound("Client not found, nothing deleted"));
-                ok = true;
-            } catch (NumberFormatException | IOException ex) {
-                System.out.println("CNP format not correct");
-            }
+        while (!ok) try {
+            System.out.print("Give client CNP:");
+            cnp = Long.parseLong(reader.readLine());
+            final long cnp2=cnp;
+            CompletableFuture.runAsync(() -> {
+                try {
+                    Optional<Client> mo = clientService.deleteClient(cnp2);
+                    mo.ifPresent(c -> System.out.println("Client deleted"));
+                    mo.orElseThrow(() -> new ClientNotFound("Client not found, nothing deleted"));
+                } catch (ClientNotFound cnf){
+                    System.out.println(cnf.getMessage());
+                }
+            });
+            ok = true;
+        } catch (NumberFormatException | IOException ex) {
+            System.out.println("CNP format not correct");
         }
     }
 
     private void listAllRentals() {
-        rentalService.getAllRentals().forEach(System.out::println);
+        CompletableFuture.runAsync(()->{
+            rentalService.getAllRentals().forEach(System.out::println);
+        });
     }
 
     private void addRental() {
@@ -347,12 +400,15 @@ public class Console {
                 System.out.println("Invalid date. Try again.");
             }
         }
-        Optional<Rental> rental = rentalService.save(cnp, mID, startDate, days);
-        if (rental.isPresent()) {
-            System.out.println("Rental already present.");
-        } else {
-            System.out.println("Rental added successfully.");
-        }
+        final LocalDate startDate2=startDate;
+        CompletableFuture.runAsync(()->{
+            Optional<Rental> mo=rentalService.save(cnp, mID, startDate2, days);
+            if (mo.isPresent()) {
+                System.out.println("Rental already present.");
+            } else {
+                System.out.println("Rental added successfully.");
+            }
+        });
     }
 
     private int readInt(String message) {
@@ -402,51 +458,64 @@ public class Console {
 
     private void filterMoviesRating() {
         float rating = readFloat("Give rating(>):");
-        Set<Movie> m=movieService.filterCustom(filterMR(rating));
-        Optional.of(m.size()).filter(s->s==0).ifPresent(x->System.out.println("No movies above that rating"));
-        m.forEach(System.out::println);
+
+        CompletableFuture.runAsync(()->{
+            Set<Movie> mo=movieService.filterCustom(filterMR(rating));
+            Optional.of(mo.size()).filter(s->s==0).ifPresent(x->System.out.println("No movies above that rating"));
+            mo.forEach(System.out::println);
+        });
     }
 
     private void filterMoviesGenre() throws IOException {
         System.out.println("Give genre: ");
         String genre = reader.readLine();
-        Set<Movie> m=movieService.filterCustom(filterMG(genre));
-        Optional<Integer> o=Optional.of(m.size()).filter(s->s==0);
-        o.ifPresent(x->System.out.println("No movies with that genre"));
-        m.forEach(System.out::println);
+
+        CompletableFuture.runAsync(()->{
+            Set<Movie> mo=movieService.filterCustom(filterMG(genre));
+            Optional<Integer> o=Optional.of(mo.size()).filter(s->s==0);
+            o.ifPresent(x->System.out.println("No movies with that genre"));
+            mo.forEach(System.out::println);
+        });
     }
 
     private void filterClientsAge() throws IOException {
         int age = readInt("Give age:");
         System.out.println("Specify if you want clients younger or older than the age(>, <):");
         String sign = reader.readLine();
-        Set<Client> c=clientService.filterCustom(filterCA(age,sign));
-        Optional.of(c.size()).filter(s->s==0).ifPresent(x->System.out.println("No clients above/below that age"));
 
-        c.forEach(ce -> System.out.println(ce + "\n"));
+        CompletableFuture.runAsync(()->{
+            Set<Client> c=clientService.filterCustom(filterCA(age,sign));
+            Optional.of(c.size()).filter(s->s==0).ifPresent(x->System.out.println("No clients above/below that age"));
+            c.forEach(ce -> System.out.println(ce + "\n"));
+        });
     }
 
     private void filterRentalDuration() throws IOException {
         int duration = readInt("Give duration:");
         System.out.println("Specify if you want rentals' with less or more than the given duration(>, <):");
         String sign = reader.readLine();
-        Set<Rental> re=rentalService.filterCustom(filterRD(duration,sign));
-        Optional.of(re.size()).filter(s->s==0).ifPresent(x->System.out.println("No rentals above/below that duration"));
-
-        re.forEach(r -> System.out.println(r + "\n"));
+        CompletableFuture.runAsync(()->{
+            Set<Rental> re=rentalService.filterCustom(filterRD(duration,sign));
+            Optional.of(re.size()).filter(s->s==0).ifPresent(x->System.out.println("No rentals above/below that duration"));
+            re.forEach(r -> System.out.println(r + "\n"));
+        });
     }
     private void filterRentalClient(){
         long cID = readLong("Give client CNP:");
-        Set<Rental> re=rentalService.filterCustom(filterRC(cID));
-        Optional.of(re.size()).filter(s->s==0).ifPresent(x->System.out.println("No rentals for that client"));
-        re.forEach(c -> System.out.println(c + "\n"));
+
+        CompletableFuture.runAsync(()->{
+            Set<Rental> re=rentalService.filterCustom(filterRC(cID));
+            Optional.of(re.size()).filter(s->s==0).ifPresent(x->System.out.println("No rentals for that client"));
+            re.forEach(c -> System.out.println(c + "\n"));
+        });
     }
     private void filterRentalMovie(){
         int mID = readInt("Give movie ID: ");
-        Set<Rental> re=rentalService.filterCustom(filterRM(mID));
-        Optional.of(re.size()).filter(s->s==0).ifPresent(x->System.out.println("No rentals for that movie"));
-
-        re.forEach(System.out::println);
+        CompletableFuture.runAsync(()->{
+            Set<Rental> re=rentalService.filterCustom(filterRM(mID));
+            Optional.of(re.size()).filter(s->s==0).ifPresent(x->System.out.println("No rentals for that movie"));
+            re.forEach(System.out::println);
+        });
     }
 }
 
